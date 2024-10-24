1ee68b6e5226937a9251a8e8c4f67c407f64d0de
klatif <klatif@ncsu.edu>
2024-10-24 09:08:55 -0400

    Corrected implementation of Timer.
    Appears last implementation had inconsistencies and would not work. Tested this implementation on my local machine, appears to work as expected.
    
    More testing may be a good idea b/c the only place this is used (currently) is PID, so we won't be able to tell if it works or not.

--- High_Stakes/src/math/PID.cpp
+++ High_Stakes/src/math/PID.cpp
@@ -13,7 +13,7 @@ double PID::calculate(const double error) {
 
     // Calculate the time elapsed since the last call
     const double delta_time = this->timer.get_duration();
-    this->timer.reset();  // Reset the timer for the next iteration
+    this->timer.start();  // Reset the timer for the next iteration
 
     // Calculate the PID outputs
     const double p = this->calculate_proportional(error);
