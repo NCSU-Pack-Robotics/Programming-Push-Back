cbd07558296649748a1359b1ce8e6f0c19bd7b32
klatif <klatif@ncsu.edu>
2024-10-24 08:12:08 -0400

    Refactored PID for clarity.
    Functionality remains identical.

--- High_Stakes/src/math/PID.hpp
+++ High_Stakes/src/math/PID.hpp
@@ -1,18 +1,26 @@
 #pragma once
 
-#include "Timer.hpp"
+#include "../Timer.hpp"
 
 /**
- * P: Proportional
- * I: Integral
- * D: Derivative
+ * A PID controller is a control loop feedback mechanism used to minimize some error
+ * over time by adjusting a control input based on the error value.
+ * Popular in robotics, a PID controller is used to control the speed of motors.
+ *
+ * P: Proportional  (compensates for present error—absolutely essential)
+ * I: Integral  (compensates for steady state error—very common to ignore)
+ * D: Derivative  (compensates for overshoot—often optional but useful)
  */
 class PID {
-protected:
+private:
+    /** Proportional coefficient */
     const double k_proportional;
+    /** Integral coefficient */
     const double k_integral;
+    /** Derivative coefficient */
     const double k_derivative;
 
+    /** Timer to calculate elapsed time between calls */
     Timer timer;
     bool first_loop = true;
 
@@ -28,7 +36,8 @@ public:
      * @param k_integral Integral coefficient
      * @param k_derivative Derivative coefficient
      */
-    constexpr PID(const double k_proportional, const double k_integral, const double k_derivative): k_proportional(k_proportional), k_integral(k_integral), k_derivative(k_derivative) {}
+    constexpr PID(const double k_proportional, const double k_integral, const double k_derivative)
+        : k_proportional(k_proportional), k_integral(k_integral), k_derivative(k_derivative) {}
 
     /**
      * Calculate new output from given error
@@ -36,4 +45,28 @@ public:
      * @return PID output
      */
     double calculate(double error);
+
+private:
+    /**
+     * Calculate the proportional term
+     * @param error Displacement from target state
+     * @return Proportional term
+     */
+    double calculate_proportional(const double error);
+
+    /**
+     * Calculate the integral term
+     * @param error Displacement from target state
+     * @param delta_time Time elapsed since last call
+     * @return Integral term
+     */
+    double calculate_integral(const double error, const double delta_time);
+
+    /**
+     * Calculate the derivative term
+     * @param error Displacement from target state
+     * @param delta_time Time elapsed since last call
+     * @return Derivative term
+     */
+    double calculate_derivative(const double error, const double delta_time);
 };
