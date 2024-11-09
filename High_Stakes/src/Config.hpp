e7e40a457c2afe33c87eaabf2bc7b3e993ab4406
miles352 <milestdaly@icloud.com>
2024-11-08 19:46:29 -0500

    forgot to commit config file

---
+++ High_Stakes/src/Config.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+
+
+namespace Constants {
+    namespace MotorSpeeds {
+        /** Motor speed of the intake going inwards */
+        constexpr int32_t INTAKE_INWARDS = 70;
+        /** Motor speed of the intake going outwards */
+        constexpr int32_t INTAKE_OUTWARDS = -70;
+    }
+    
+}
