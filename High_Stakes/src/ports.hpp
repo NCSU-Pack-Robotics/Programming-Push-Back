88f86239abe0b8c1004a9e85a8e603ae30f41d21
miles352 <milestdaly@icloud.com>
2024-11-10 20:14:42 -0500

    Changed todo in ports.hpp and added comments for what robot ports are for

--- High_Stakes/src/ports.hpp
+++ High_Stakes/src/ports.hpp
@@ -9,6 +9,9 @@
 namespace Ports {
     // Note: negative values reverse the motor's direction
 
+    //TODO: Change ports to competition robot ports
+
+    // These ports are for the non-competition robot
     /** Port of the left front drive motor */
     constexpr std::int8_t LEFT_FRONT_MOTOR_PORT = -11;
     /** Port of the left back drive motor */
@@ -18,8 +21,8 @@ namespace Ports {
     /** Port of the right back drive motor */
     constexpr std::int8_t RIGHT_BACK_MOTOR_PORT = 21;
 
-    // TODO: Find correct port
 
+    // This port is for the competition robot
     /** Port of the lift motor */
     constexpr std::int8_t LIFT_MOTOR_PORT = 1;
 
