a00059eda3f2843e3d590345430a430719aa953a
klatif <klatif@ncsu.edu>
2025-02-08 15:59:22 -0500

    Preliminary auton.

--- High_Stakes/src/AutonomousControlScheduler.cpp
+++ High_Stakes/src/AutonomousControlScheduler.cpp
@@ -1,20 +1,32 @@
 #include "AutonomousControlScheduler.hpp"
 
 #include "commands/PurePursuit.hpp"
-#include "commands/DriveStraight.hpp"
 #include "asset.hpp"
+#include "commands/Default/MoveLiftDegrees.hpp"
+#include "subystems/Lift.hpp"
+#include "subystems/Clamp.hpp"
 
 // Global paths defined here
-ASSET(straight72_txt);
-ASSET(back72_txt);
+ASSET(getGoal_txt);
+ASSET(getFirstRing_txt);
+
+using namespace std;
 
 AutonomousControlScheduler::AutonomousControlScheduler(): ChainCommand({}) {
+    Clamp& clamp = AbstractSubsystem::get_instance<Clamp>();
+    Lift& lift = AbstractSubsystem::get_instance<Lift>();
+    Drivetrain& drivetrain = AbstractSubsystem::get_instance<Drivetrain>();
+
     // Add commands to the chain here
-    // add_command_and(std::make_unique<PurePursuit>(straight72_txt)).
-    //     add_command_and(std::make_unique<InstantCommand>(std::make_unique<std::function<void()>>([&] { drivetrain.set_reversing(true); }))).
-    //     add_command_and(std::make_unique<PurePursuit>(back72_txt)).
-    //     add_command(std::make_unique<InstantCommand>(std::make_unique<std::function<void()>>([&] {drivetrain.set_reversing(false); })));
-    add_command(std::make_unique<DriveStraight>(72.0, 1));
+    add_command_and(std::make_unique<InstantCommand>(std::make_unique<std::function<void()>>(
+        [&] { drivetrain.set_reversing(true); }))).
+    add_command_and(make_unique<PurePursuit>(getGoal_txt)).
+    add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
+        [&] { clamp.set_enabled(true); }))).
+    add_command_and(make_unique<MoveLiftDegrees>(1600)).
+    add_command_and(make_unique<PurePursuit>(getFirstRing_txt)).
+    add_command_and(make_unique<MoveLiftDegrees>(1600));
+
 }
 
 void AutonomousControlScheduler::initialize() {
