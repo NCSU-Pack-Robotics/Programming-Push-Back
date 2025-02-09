78e1e8d34cd9285026677b966f1b7ee9c65b8082
klatif <klatif@ncsu.edu>
2025-02-09 16:01:44 -0500

    Touching Post.

--- High_Stakes/src/AutonomousControlSchedulerDo.cpp
+++ High_Stakes/src/AutonomousControlSchedulerDo.cpp
@@ -3,12 +3,15 @@
 #include "asset.hpp"
 #include "commands/PPIntakeLift.hpp"
  #include "commands/PurePursuit.hpp"
+#include "commands/Default/MoveLadyBrownPosition.hpp"
 #include "subystems/Clamp.hpp"
 #include "subystems/Lift.hpp"
+#include "subystems/LadyBrown.hpp"
 
 // Global paths defined here
 ASSET(getGoal_txt);
 ASSET(getFirstRing_txt);
+ASSET(touchPost_txt);
 
 using namespace std;
 
@@ -17,7 +20,9 @@ AutonomousControlSchedulerDo::AutonomousControlSchedulerDo(): ChainCommand({}) {
     Lift& lift = AbstractSubsystem::get_instance<Lift>();
     Intake& intake = AbstractSubsystem::get_instance<Intake>();
     Drivetrain& drivetrain = AbstractSubsystem::get_instance<Drivetrain>();
+    LadyBrown& lady_brown = AbstractSubsystem::get_instance<LadyBrown>();
 
+    // Add commands to the chain here
     // Add commands to the chain here
     add_command_and(std::make_unique<InstantCommand>(std::make_unique<std::function<void()>>(
         [&] { drivetrain.set_reversing(true); }))).  // Reverse the drivetrain
@@ -25,14 +30,18 @@ AutonomousControlSchedulerDo::AutonomousControlSchedulerDo(): ChainCommand({}) {
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
         [&] { clamp.set_enabled(true); }))); // Enable the clamp
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
-        [&] { pros::delay(100); }))).  // Pause for a second
+        [&] { pros::delay(1000); }))).  // Pause for a second
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
         [&] { drivetrain.set_reversing(false); }))).  // Stop reversing the drivetrain
     add_command_and(make_unique<PPIntakeLift>(getFirstRing_txt)).  // Follow path, intake, and lift ring
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
-        [&] { pros::delay(1500); }))).  // Pause for a second
+        [&] { pros::delay(2500); }))).  // Pause for a second
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
-        [&] { lift.brake(); intake.brake(); })));
+        [&] { lift.brake(); intake.brake(); }))).  // Stop intake and lift
+    add_command_and(make_unique<MoveLadyBrownPosition>(LadyBrown::Position::SCORE)).  // Raise LadyBrown
+    add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
+        [&] { lady_brown.set_killed(true); }))).  // Stop intake and lift
+    add_command_and(make_unique<PurePursuit>(touchPost_txt));  // Go to clib bar
 }
 
 void AutonomousControlSchedulerDo::initialize() {
--- High_Stakes/src/AutonomousControlSchedulerThink.cpp
+++ High_Stakes/src/AutonomousControlSchedulerThink.cpp
@@ -2,6 +2,7 @@
 
 #include "commands/PPIntakeLift.hpp"
 #include "asset.hpp"
+#include "subystems/LadyBrown.hpp"
 #include "commands/PurePursuit.hpp"
 #include "subystems/Clamp.hpp"
 #include "subystems/Lift.hpp"
@@ -9,6 +10,7 @@
 // Global paths defined here
 ASSET(getGoalThink_txt);
 ASSET(getFirstRingThink_txt);
+ASSET(touchPostThink_txt);
 
 using namespace std;
 
@@ -17,6 +19,7 @@ AutonomousControlSchedulerThink::AutonomousControlSchedulerThink(): ChainCommand
     Lift& lift = AbstractSubsystem::get_instance<Lift>();
     Intake& intake = AbstractSubsystem::get_instance<Intake>();
     Drivetrain& drivetrain = AbstractSubsystem::get_instance<Drivetrain>();
+    LadyBrown& lady_brown = AbstractSubsystem::get_instance<LadyBrown>();
 
     // Add commands to the chain here
     add_command_and(std::make_unique<InstantCommand>(std::make_unique<std::function<void()>>(
@@ -30,9 +33,16 @@ AutonomousControlSchedulerThink::AutonomousControlSchedulerThink(): ChainCommand
         [&] { drivetrain.set_reversing(false); }))).  // Stop reversing the drivetrain
     add_command_and(make_unique<PPIntakeLift>(getFirstRingThink_txt)).  // Follow path, intake, and lift ring
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
-        [&] { pros::delay(5000); }))).  // Pause for a second
+        [&] { pros::delay(2500); }))).  // Pause for a second
     add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
-        [&] { lift.brake(); intake.brake(); })));
+        [&] { lift.brake(); intake.brake(); }))).  // Stop intake and lift
+    add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
+        [&] { lady_brown.set_position(LadyBrown::Position::SCORE); }))).  // Raise LadyBrown
+    add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
+        [&] { pros::delay(1000); }))).  // Pause for a second
+    add_command_and(make_unique<InstantCommand>(make_unique<function<void()>>(
+        [&] { lady_brown.set_killed(true); }))).  // Stop intake and lift
+    add_command_and(make_unique<PurePursuit>(touchPostThink_txt));  // Go to clib bar
 
 }
 
