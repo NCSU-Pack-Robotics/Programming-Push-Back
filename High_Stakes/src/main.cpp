a0710ffec13c3bc8e011cf9a1e0dc9c0847f5fee
mtdaly <mtdaly@ncsu.edu>
2025-03-23 13:25:06 -0400

    Reverted change to autonomous main loop because it would cause unintended side effects.

--- High_Stakes/src/main.cpp
+++ High_Stakes/src/main.cpp
@@ -88,8 +88,8 @@ void autonomous() {
 
     scheduler.initialize();
 
-    // Run while the autonomous routine is not complete.
-    while (!scheduler.is_complete()) {
+    // Run forever
+    while (true) {
         // Run the autonomous scheduler to do our routine
         scheduler.run();
 
