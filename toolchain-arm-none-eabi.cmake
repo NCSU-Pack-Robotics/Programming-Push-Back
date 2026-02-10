8f8681eff5626a26f28b7b0887145f060488bbf8
KorayL <i.koray@me.com>
2026-02-10 17:05:49 -0500

    Project compiles
    - Builds entire project into one executable
    - More work for hot/cold linking and timestamp
    - Run with the following command: ` rm -rf build && cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=toolchain-arm-none-eabi.cmake -DBRAIN=1 -DTHINK=1 && cmake --build build -j 14`

---
+++ toolchain-arm-none-eabi.cmake
@@ -0,0 +1,25 @@
+# Cross compilation toolchain setup
+
+# Tell CMake we're cross-compiling for ARM architecture
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Set the toolchain for cross-compilation
+set(TOOLCHAIN_PREFIX arm-none-eabi-)
+
+# Compilers
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+
+# Utils
+set(CMAKE_AR ${TOOLCHAIN_PREFIX}ar)
+set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Skip compiler test for cross-compilation
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
