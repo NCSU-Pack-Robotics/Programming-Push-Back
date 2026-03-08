95817331a75584c08b562cb4acbd909ae7b72314
Koray <klatif@ncsu.edu>
2026-03-08 15:12:16 -0400

    `CommandImpl` now it's own file called `LogCommand`

--- Push_Back/test/src/architecture/commands/CommandTest.cc
+++ Push_Back/test/src/architecture/commands/CommandTest.cc
@@ -2,47 +2,14 @@
 #include <gtest/gtest.h>
 
 #include "architecture/commands/Command.hpp"
-
-/** An implementation of <code>Command</code> to make testing possible. */
-class CommandImpl : public Command {
-public:
-    /** Vector of strings to keep track of which methods were run. */
-    std::vector<std::string>& log;
-
-    /**
-     * @param log Vector of strings to keep track of which methods were run.
-     * @param lifespan The number of calls to run() before the command is complete.
-     */
-    explicit CommandImpl(auto& log, const int lifespan) : log(log), lifespan(lifespan) {
-    }
-
-    bool is_complete() override {
-        return calls >= lifespan;
-    }
-
-    void initialize() override {
-        this->log.emplace_back("initialize");
-    }
-
-    void periodic() override {
-        this->log.push_back(std::format("periodic_{}", calls - 1));
-    }
-
-    void shutdown() override {
-        this->log.emplace_back("shutdown");
-    }
-
-private:
-    /** The number of calls to periodic() before the command is complete. */
-    const int lifespan;
-};
+#include "helpers/LogCommand.cc"
 
 /**
  * Smoke test: Most popular use case: a command that runs for a few loops before completing.
  */
 TEST(CommandTest, smokeTest) {
     std::vector<std::string> log;
-    const std::unique_ptr<Command> cmd = std::make_unique<CommandImpl>(log, 6);
+    const std::unique_ptr<Command> cmd = std::make_unique<LogCommand>(log, 6);
 
     // Initialize and run periodic 0
     cmd->run();
@@ -85,7 +52,7 @@ TEST(CommandTest, smokeTest) {
  */
 TEST(CommandTest, completeOnFirstRun) {
     std::vector<std::string> log;
-    const std::unique_ptr<Command> cmd = std::make_unique<CommandImpl>(log, 0);
+    const std::unique_ptr<Command> cmd = std::make_unique<LogCommand>(log, 0);
 
     cmd->run();
     ASSERT_EQ(log.size(), 1);
